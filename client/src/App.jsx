b177beb6d2e91b0cedc8a4e1637518e463724e7e
Doc-Franz <federico.franzi94@gmail.com>
2025-05-09 21:29:14 +0200

    add organizations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import MyNav from "./components/MyNav";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return <MyNav />;
--- client/src/components/MyNav.jsx
+++ client/src/components/MyNav.jsx
@@ -1,16 +1,46 @@
-import { Row } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
 function MyNav() {
+  // fetch per aggiungere le aziende in piattaforma
+  async function addOrganization() {
+    try {
+      await fetch("api/organizations", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name: "new organization" })
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
-    <Container>
-      <Row>
-        <p>ciao</p>
-      </Row>
-    </Container>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <Button onClick={addOrganization}>organization</Button>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
